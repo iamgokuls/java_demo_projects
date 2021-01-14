476840dddd910e3f7a0b83b61f1f61af11248b4c
sreejamadire1 <sreeja.madire@incture.com>
2021-01-14 14:17:47 +0530

    mine is 900 gokuls 800

--- Java_Demo_Projects/src/EmailApp/EmailApp.java
+++ Java_Demo_Projects/src/EmailApp/EmailApp.java
@@ -4,12 +4,7 @@ public class EmailApp {
 	
 	public static void main(String[] args) {
 		
-		Email em=new Email("Gokul","Suresh");
-		
-		//em.setAltEmail("sjfhlaks");
-		//em.setMaxCapacity(700);
-		//em.setPassword("sdkfh");
-		
+		new Email("Gokul","Suresh");
 	}
 
 }
